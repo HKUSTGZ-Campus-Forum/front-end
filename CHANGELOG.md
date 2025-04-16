094bc39b51a9d1f383c782dc699bcd9a677d8ccf
Yomi017 <thuang017@connect.hkust-gz.edu.cn>
2025-04-16 22:05:00 +0800

    [update] 0.1.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,9 @@
 All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
 
 ### [0.1.2-0](https://github.com/HKUSTGZ-Campus-Forum/front-end/compare/v0.1.1...v0.1.2-0) (2025-04-16)
+修复了JWT令牌的问题
+现在可以发帖和看帖子
+图片上传功能实现了上传，还没能对应到帖子上
 
 ### 0.1.1 (2025-04-13)
 "更新了自动化版本管理",
--- version.ts
+++ version.ts
@@ -1,6 +1,6 @@
 // 版本号定义
-export const version = "0.1.1";
-export const buildDate = "2025-04-14";
+export const version = "0.1.2";
+export const buildDate = "2025-04-16";
 
 export const versionHistory = [
   {
@@ -17,5 +17,14 @@ export const versionHistory = [
     version: "0.1.1",
     date: "2025-04-14",
     changes: ["更新了自动化版本管理"],
+  },
+  {
+    version: "0.1.2",
+    date: "2025-04-16",
+    changes: [
+      "修复了JWT令牌的问题",
+      "现在可以发帖和看帖子",
+      "图片上传功能实现了上传，还没能对应到帖子上",
+    ],
   },
 ];
